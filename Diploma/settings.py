67d660cf727b7503926a2f61c1b5c88910b9d102
hushchynd <hushchynd@gmail.com>
2023-05-09 21:13:17 +0300

    Remove a hardcoded Django secret key

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6v-km5ek+98hkvq^wf!*h1yiyg6lqeknkixqj%m^-j-#4k@y+u'
+# SECRET_KEY = 'django-insecure-6v-km5ek+98hkvq^wf!*h1yiyg6lqeknkixqj%m^-j-#4k@y+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
