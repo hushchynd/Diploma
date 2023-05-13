26e7d27cd10811665309de5ee596e16a70ce5613
hushchynd <hushchynd@gmail.com>
2023-05-13 12:35:50 +0300

    start19

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -183,7 +183,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
