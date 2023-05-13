d1ced83acfa8a39fe7e20f65af1fec1e5703e2ba
hushchynd <hushchynd@gmail.com>
2023-05-13 21:33:43 +0300

    start20

--- kino_app/static/kino_app/base.js
+++ kino_app/static/kino_app/base.js
@@ -2,7 +2,7 @@
 $('body').each(function (index,elem) {
 
     $(elem).css({
-        'background':` url(${$(elem).data('background')}) center/100% 100% no-repeat`,
+        'background':` url('${$(elem).data('background')}') center/100% 100% no-repeat`,
         'background-attachment':`fixed`,
     })
 })
