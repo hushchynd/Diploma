acded8a735005d472991eb6705c498f68ec589d2
hushchynd <hushchynd@gmail.com>
2023-05-13 09:23:00 +0300

    start11

--- Diploma/urls.py
+++ Diploma/urls.py
@@ -33,14 +33,13 @@ urlpatterns = i18n_patterns(
     path('', include('kino_app.urls')),
 
 )
-urlpatterns = [
-                  ] + urlpatterns + static(proj.MEDIA_URL, document_root=proj.MEDIA_ROOT)
+
 if proj.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
                       path('__debug__/', include(debug_toolbar.urls)),
-                  ]
+                  ] + urlpatterns + static(proj.MEDIA_URL, document_root=proj.MEDIA_ROOT)
 
 # from django.contrib.staticfiles.views import serve
 # from django.views.static import serve as media_serve
